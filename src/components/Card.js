57038bc315f49a0a8f1f961e45fb69a50708e52a
Jady Rufino <jadyrufa@gmail.com>
2020-06-07 18:12:56 -0300

    definicao da estrutura do app

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,6 +3,39 @@ import moment from 'moment';
 
 export default function Card({day}){
 
+  const dateConstructor = (data) => {
+    const mounths = [
+      'Janeiro',
+      'Favereiro',
+      'Março',
+      'Abril',
+      'Maio',
+      'Junho',
+      'Julio',
+      'Agosto',
+      'Setembro',
+      'Outubro',
+      'Novembro',
+      'Dezembro',
+    ];
+    const days = [
+      'Segunda-Feira',
+      'Terça-Feira',
+      'Quarta-Feira',
+      'Quinta-Feira',
+      'Sexta-Feira',
+      'Sábado',
+      'Domingo',
+    ];
+
+    const day = days[data.getDay()];
+    const date = data.getDate();
+    const month = mounths[data.getMonth()];
+    const year = data.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
     let newDate = new Date();
     const weekday = day.dt * 1000
     newDate.setTime(weekday)
@@ -15,10 +48,11 @@ export default function Card({day}){
         {/* Informações do tempo */}
         <div className="WeatherDesc-box">
           <div lassName="location">
-            <div className="dateWeather">{moment(newDate).locale('pt-br').format('dddd')}</div>
             <i className={imgURL}></i>
             <h2 className="temperature">{Math.round(day.main.temp)} °C</h2>
             <div  className="weather">{day.weather[0].description}
+            <div className="dateWeather">{dateConstructor(newDate)}</div>
+
                 <ul className="weatherLis">
                     <li>Vento: {day.wind.speed} km/h</li>
                     <li>Pressão: {day.main.temp} hpa</li>
